f5bc1156f5ac99f0a75ec585f8cb02218bc73eec
Kenneth Powers <mail@kenpowers.net>
2012-03-20 18:00:26 -0400

    all: Server now prints out requested file path and mime type.

--- lib/ws.rb
+++ lib/ws.rb
@@ -1,3 +1,16 @@
+require "socket"
+
 module WS
-	#todo
+	def self.start_server(options)
+		server = TCPServer.new("127.0.0.1", options[:port])
+		while (conn = server.accept)
+			file = conn.gets.scan(/(?<=\/).*(?=\sHTTP)/)[0].split("/")
+			file_path = File.join(options[:directory], file)
+			file_path = File.join(file_path, "index.html") if File.directory? file_path
+			mime_type = get_mime_type file_path
+			conn.puts mime_type
+			conn.puts file_path
+			conn.close
+		end
+	end
 end
