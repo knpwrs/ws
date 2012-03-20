ec836a84221c2d444a24aabc2b7b8bf7739deb58
Kenneth Powers <mail@kenpowers.net>
2012-03-20 18:18:57 -0400

    Server complete.

--- lib/ws.rb
+++ lib/ws.rb
@@ -8,9 +8,20 @@ module WS
 			file_path = File.join(options[:directory], file)
 			file_path = File.join(file_path, "index.html") if File.directory? file_path
 			mime_type = get_mime_type file_path
-			conn.puts mime_type
-			conn.puts file_path
+			begin
+				file_handle = File.open(file_path, "r")
+				conn.print get_header("200/OK", mime_type)
+				conn.print file_handle.read()
+			rescue Errno::ENOENT
+				conn.print get_header("404/NOT FOUND", "text/html")
+				conn.print "404: File not found."
+			end
 			conn.close
 		end
+	end
+
+	private
+	def self.get_header(status, mime_type)
+		"HTTP/1.1 #{status}\r\nContent-Type:#{mime_type}\r\n\r\n"
 	end
 end
